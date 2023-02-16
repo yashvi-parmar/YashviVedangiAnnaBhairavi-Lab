5b4ac6f63b73e74bde93bb98c2da7f988d900bb2
Bhairavi Thalayasingam <bthalaya@uwaterloo.ca>
2023-02-16 15:34:11 -0500

    final push

--- src/components/RestaurantList.spec.js
+++ src/components/RestaurantList.spec.js
@@ -3,24 +3,29 @@ import RestaurantList from './RestaurantList';
 
 
 describe('RestaurantList', () => {
-  it('loads restaurants on first render', () => {
-    const loadRestaurants = jest.fn().mockName('loadRestaurants');
-    const restaurants = [];
-    render(
-        <RestaurantList
-          loadRestaurants={loadRestaurants}
-          restaurants={restaurants}
-        />,
-    );
-    expect(loadRestaurants).toHaveBeenCalled();
-}); 
-
-it('displays the restaurants', () => { const noop = () => {};
     const restaurants = [
         {id: 1, name: 'Sushi Place'},
         {id: 2, name: 'Pizza Place'},
-    ];
-    render(<RestaurantList loadRestaurants={noop} restaurants={restaurants} />);
+      ];
+      let loadRestaurants;
+      function renderComponent() {
+        loadRestaurants = jest.fn().mockName('loadRestaurants');
+        render(
+          <RestaurantList
+            loadRestaurants={loadRestaurants}
+            restaurants={restaurants}
+          />,
+    ); }
+
+
+  it('loads restaurants on first render', () => {
+    renderComponent();
+    expect(loadRestaurants).toHaveBeenCalled();
+}); 
+
+it('displays the restaurants', () => { 
+
+    renderComponent();
 
     expect(screen.getByText('Sushi Place')).toBeInTheDocument();
     expect(screen.getByText('Pizza Place')).toBeInTheDocument();
